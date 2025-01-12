60e940771cd3fdb023d9031b423438fe0c5301e3
ryaalbr <63967422+ryaalbr@users.noreply.github.com>
2025-01-12 07:33:13 -0800

    Update step_type.sh

--- step_type.sh
+++ step_type.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python extract_step_type.py \
-    --model_name_or_path openlm-research/open_llama_7b_v2 \
+    --model_name_or_path openlm-research/open_llama_3b_v2 \
     --batch_size 1 \
     --dataset gsm8k \
     --selection_method vae \
