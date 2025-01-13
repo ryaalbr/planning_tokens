b62a4caf72970ab821f0d4cda5ab24f37ea09aa5
ryaalbr <63967422+ryaalbr@users.noreply.github.com>
2025-01-13 15:47:13 -0800

    Update step_type.sh

--- step_type.sh
+++ step_type.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python extract_step_type.py \
+python extract_step_type.py \
     --model_name_or_path openlm-research/open_llama_3b_v2 \
     --batch_size 1 \
     --dataset gsm8k \
